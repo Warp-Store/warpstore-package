a522c16f6b49969e24b7b6c8d81eebbc61b3b6e7
Daniel-D3V <personal.danielsilva.d3v@gmail.com>
2024-02-15 18:33:19 -0300

    ave

--- README.md
+++ README.md
@@ -1,7 +1,8 @@
 # warpstore-package
 
 Esta biblioteca oferece aos desenvolvedores uma coleção descomplicada de funcionalidades para facilitar a integração a API da Warp Store em seu site, permitindo a exibição e venda de produtos cadastrados no nosso Dashboard.
-## 📲 Installation 
+
+## 📲 Instalação 
 
 1. Instale o nosso NODE SDK usando o comando logo abaixo:
 ```sh
@@ -28,33 +29,3 @@ if(response.isFailure()){
 
 console.log(response.value);
 ```
-
-## ✨ Logic
-
-  Each operation will yield one of two outcomes: a MercadoPagoError in the event of an error, or the desired value in case of a successful operation.
-
-  You can check if the operation was successfull or not by calling the methods:
-```javascript
-const payment = await mercadoPago.value.payment.create()
-if(payment.isFailure()) console.log("operation failed")
-if(payment.isSuccess()) console.log("operation succeeded")
-```
-Definition of MercadoPagoError:
-```javascript
-class MercadoPagoError {
-  message: string;
-  aditionalInfo: any;
-  status: number;
-  code: number | string
-}
-```
-response if successfull:
-```javascript
-const response =  {
-  id: string
-  payment_method_id: string
-  // ...
-}
-```
-
-  Keep in mind the responses are not typed, so you will have to look at the documentation to consult what each operation will return.
--- src/lib/request-manager.ts
+++ src/lib/request-manager.ts
@@ -3,34 +3,34 @@ import { Either, failure, success } from "@/core/logic";
 import axios, { AxiosResponse } from "axios"
 
 const api = axios.create({
-    baseURL: 'http://localhost:5000',
+    baseURL: 'https://api.warpstore.app',
     timeout: 1000,
     validateStatus: () => true
 })
 
 export class RequestManager {
 
-    static async makeRequest<TResponse, TError, >(path: string, httpInput: RequestManager.HttpInput = {} as any): Promise<Either<TError, TResponse>> {
+    static async makeRequest<TResponse, TError,>(path: string, httpInput: RequestManager.HttpInput = {} as any): Promise<Either<TError, TResponse>> {
         const { body, headers, query, method } = httpInput
 
         let response: AxiosResponse
 
-        if(method === "POST") {
+        if (method === "POST") {
             response = await api.post(path, body, {
                 headers,
                 params: query
             })
-        }else {
+        } else {
             response = await api.get(path, {
                 headers,
                 params: query
             })
         }
 
-        if(response.status !== 200) {
+        if (response.status !== 200) {
             return failure(new CustomDomainError(response.data?.error?.name)) as any
         }
-        
+
         return success(response.data) as any
     }
 
@@ -40,7 +40,7 @@ export namespace RequestManager {
 
 
     export type HttpInput = {
-        method: "GET" | "POST" 
+        method: "GET" | "POST"
         body?: object
         headers?: object
         query?: object
