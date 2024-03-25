8976968dd443cd599237b0dd1c167759ca609313
Daniel-D3V <personal.danielsilva.d3v@gmail.com>
2024-03-25 01:20:42 -0300

    finsih

--- src/index.ts
+++ src/index.ts
@@ -5,17 +5,42 @@ import { WarpStore } from "./main"
 (async () => {
 
     const warpstore = new WarpStore()
-    WarpStore.setApiUrl("http://localhost:5000")
-    const response = await warpstore.template.v1.discordLogin.getCurrentUser({
-        storeId: "30363d97-f8d7-4dea-af2c-0451f7bca15d",
-        accessToken: "11aZi3CExHn1NTUHfShmz8esujwOdm"
+
+    const storeInfo = await warpstore.template.v1.store.getInfo({
+        subDomain: "seu_sub_dominio",
+        // domain: "ou_seu_dominio" 
     })
 
-    console.log(response.value)
-    if (response.isFailure()) {
+    if (storeInfo.isFailure()) return; // ele ignora tudo caso a loja não for encontrada.
 
-        return
+    const response = await warpstore.checkout.placeOrder({
+        storeId: storeInfo.value.id, // id da loja retornado nos dados da loja
+        customer: {
+            cpf: 'cpf cliente',
+            email: 'email cliente',
+            fullName: 'nome completo cliente',
+            phone: 'telefone cliente'
+        },
+        gameUserReference: 'id no jogo',
+        items: [
+            {
+                productId: 'id de um produto por exemplo',
+                quantity: 2, // quantidade,
+            }
+        ],
+        paymentGateway: "MERCADO_PAGO", // metodo de pagamento,
+        couponCode: 'código de cupom se o cliente usou'
+    })
+    if (response.isFailure()) {
+        // ele vai dar um failure caso não for possível criar a compra ou der um erro.
     }
 
+    console.log(response.value) /* Retorna isto: 
+        id: string
+        approved: boolean
+        qrCode?: string
+        code?: string
+        paymentLink?: string
+    */
 })()
 "sdddddsdddddddddddddddddd"
