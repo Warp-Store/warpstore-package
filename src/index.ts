fd931d67d3a3775d69ab565b97c8f4ba08eaea80
Gabriel Camargo <gagavtproducao@gmail.com>
2024-06-19 18:13:10 -0300

    :adhesive_bandage: fix: remove code tests

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,2 @@
 import { WarpStore } from "./main"
 
-
-
-(async () => {
-
-    WarpStore.setApiUrl('http://192.168.1.42:5000')
-    const warpstore = new WarpStore()
-
-    const storeInfo = await warpstore.store.v1.getInfo({
-        subDomain: "adasd",
-        // domain: "ou_seu_dominio" 
-    })
-    if (storeInfo.isFailure()) return;
-})()
-"sdddddsdddddddddddddddddd"
